2d9fa1a3225f6826ef91932f9d0041aff634baec
Siarhei Petrashenko <sergey.petrashenko@yahoo.com>
2023-08-14 18:20:33 +0200

    tabs commit

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"> <img src="Icons/chevron-left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="Icons/chevron-right-solid.png"></button>'
+      });
+  });
