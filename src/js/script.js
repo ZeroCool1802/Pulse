4c6c3f136a0dcb089a3d74a6b1899a5636606093
Siarhei Petrashenko <sergey.petrashenko@yahoo.com>
2023-08-28 23:50:37 +0200

    valid

--- src/js/script.js
+++ src/js/script.js
@@ -40,6 +40,93 @@ $(document).ready(function(){
         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
         $('.overlay,#order').fadeIn();
       })
-
     });
+
+      function valideForms(form){
+        $(form).validate({
+          rules:{
+            name:{
+              required: true,
+              minlength: 2,
+            },
+            phone: "required",
+            email: {
+              required: true,
+              email: true
+            }
+          },
+          messages: {
+            name: {
+              required: "Введите имя",
+              minlength: jQuery.validator.format("Минимум {0} символа!")
+            },
+            phone: "Введите номер телефона",
+            email: {
+              required: "Введите e-mail адрес",
+              email: "Введите корректный e-mail"
+            }
+          }
+        });
+      };
+          valideForms('#consultation-form');
+          valideForms('#consultation form');
+          valideForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+    
+    
+    $('form').submit(function(e){
+      e.preventDefault();
+      if (!$(this).valid()){
+        return;
+      };
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function(){
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+        $('form').trigger('reset');
+      });
+      return false;
+    });
+   
+    
+          //scroll
+
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 1600){
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+      $(document).ready(function(){
+        // Add smooth scrolling to all links
+        $("a").on('click', function(event) {
+      
+          // Make sure this.hash has a value before overriding default behavior
+          if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+      
+            // Store hash
+            var hash = this.hash;
+      
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+              scrollTop: $(hash).offset().top
+            }, 0, function(){
+      
+              // Add hash (#) to URL when done scrolling (default click behavior)
+              window.location.hash = hash;
+            });
+          } // End if
+        });
+      });
+    });
+
+    new WOW().init();
   });
